e6fc5f3c074cae54aaf12d0a9dca89f95449e0fc
Amit Parashar <pugga.ai@gmail.com>
2012-01-31 14:01:04 +0530

    A splitter widget added

--- simpleproject/main.cpp
+++ simpleproject/main.cpp
@@ -1,10 +1,22 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <QtGui/QSplitter>
+#include <QtGui/QPlainTextEdit>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QSplitter *splitter = new QSplitter;
+    QPlainTextEdit *edit1 = new QPlainTextEdit;
+    QPlainTextEdit *edit2 = new QPlainTextEdit;
+
+    splitter->insertWidget(0, edit1);
+    splitter->insertWidget(1, edit2);
+
+    w.setCentralWidget(splitter);
+
     w.show();
     
     return a.exec();
