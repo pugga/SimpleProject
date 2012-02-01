6e7e1f636c6b81aaee1a93d2e6b59ccb16194a07
Amit Parashar <pugga.ai@gmail.com>
2012-02-01 10:05:28 +0530

    master of file system shown in tree and list views

--- simpleproject/main.cpp
+++ simpleproject/main.cpp
@@ -2,6 +2,9 @@
 #include "mainwindow.h"
 #include <QtGui/QSplitter>
 #include <QtGui/QPlainTextEdit>
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QListView>
 
 int main(int argc, char *argv[])
 {
@@ -9,11 +12,25 @@ int main(int argc, char *argv[])
     MainWindow w;
 
     QSplitter *splitter = new QSplitter;
-    QPlainTextEdit *edit1 = new QPlainTextEdit;
-    QPlainTextEdit *edit2 = new QPlainTextEdit;
+//    QPlainTextEdit *edit1 = new QPlainTextEdit;
+//    QPlainTextEdit *edit2 = new QPlainTextEdit;
 
-    splitter->insertWidget(0, edit1);
-    splitter->insertWidget(1, edit2);
+    QFileSystemModel *model = new QFileSystemModel(splitter);
+    model->setRootPath(QDir::currentPath());
+
+    QTreeView *tree = new QTreeView;
+    tree->setModel(model);
+
+    QListView *list = new QListView;
+    list->setModel(model);
+
+    tree->setSelectionModel(list->selectionModel());
+
+    splitter->insertWidget(0, tree);
+    splitter->insertWidget(1, list);
+
+//    splitter->insertWidget(0, edit1);
+//    splitter->insertWidget(1, edit2);
 
     w.setCentralWidget(splitter);
 
